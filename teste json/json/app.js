3e84484b458bb89aa90f3ed74f04b5e218cc9b4e
Otavio Chaves <otaviocs2005@gmail.com>
2024-04-27 20:54:19 -0300

    teste json

---
+++ teste json/json/app.js
@@ -0,0 +1,45 @@
+/*let usuario = {
+    "nome": "Edson",
+    "email": "edson@gmail.com",
+    "categoria": "coletor",
+    "senha": "12345"
+}
+
+let demanda = {
+    "tipo-lixo": "plastico",
+    "quantidade": "10kg",
+    "horario": "17:00 as 19:00",
+    "endereço": "rua 123 bairro savassi"
+}
+
+
+
+
+let usuario = {
+    "cliente": [
+        {
+            "nome": "Edson",
+            "email": "edson@gmail.com",
+            "categoria": "coletor",
+            "senha": "12345"
+        }
+    ],
+    "demanda": [
+        {
+            "tipo-lixo": "plastico",
+            "quantidade": "10kg",
+            "horario": "17:00 as 19:00",
+            "endereço": "rua 123 bairro savassi"
+        }
+    ]
+}
+*/
+
+
+
+usuario = {
+    "nome": "Edson",
+    "email": "edson@gmail.com",
+    "categoria": "coletor",
+    "senha": "12345"
+}
